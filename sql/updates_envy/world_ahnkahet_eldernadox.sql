07b346b10b58d02e6a119eae04bd56b7d2cc248d
Envy602 <lionel.cuffel@sfr.fr>
2013-08-29 21:19:40 +0200

    Sql fix

--- sql/updates_envy/world_ahnkahet_eldernadox.sql
+++ sql/updates_envy/world_ahnkahet_eldernadox.sql
@@ -1,2 +1,2 @@
 -- Elder Nadox eggs
-UPDATE `creature_template` SET `unit_flags` = `unit_flags` |2|256|33554432, `ScriptName` = "" WHERE `entry` IN (30172, 31446, 30173, 31445);
+UPDATE `creature_template` SET `unit_flags` = `unit_flags` |2|256|33554432, `ScriptName` = "npc_nadox_eggs" WHERE `entry` IN (30172, 31446, 30173, 31445);
